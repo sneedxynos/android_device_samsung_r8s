fb0cd49479ae077f14b62bc733ee1ee5fb560981
Tim Zimmerman <tim@linux4.de>
2022-04-22 11:30:52 +0200

    x1slte: NOP SecCameraToRil::enable/disable m_sendRequest()
    * Fixes switching to different camera IDs when using OSS camera
      provider.
    
    Change-Id: If8e1350cf271c847d146e415911c81698d7d38d1
    Co-authored-by: Josip K <kelecicj@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -8,6 +8,15 @@
 
 function blob_fixup() {
     case "${1}" in
+        vendor/lib64/libexynoscamera3.so)
+            xxd -p "${2}" | tr -d \\n > "${2}".hex
+            # NOP SecCameraIPCtoRIL::enable m_sendRequest()
+            sed -i "s/140000940a000014/1f2003d50a000014/g" "${2}".hex
+            # NOP SecCameraIPCtoRIL::disable m_sendRequest()
+            sed -i "s/a8ffff970a000014/1f2003d50a000014/g" "${2}".hex
+            xxd -r -p "${2}".hex > "${2}"
+            rm "${2}".hex
+            ;;
         vendor/lib*/libsec-ril*.so)
             "${PATCHELF}" --replace-needed libril.so libril-samsung.so "${2}"
             ;;

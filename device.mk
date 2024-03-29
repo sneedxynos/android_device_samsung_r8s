cffbd2e7e7e1cdf4b41209d4ccf3c353b9df10c5
borbelyvince <borbely.vince.11@gmail.com>
2024-03-29 14:32:48 +0100

    r8s: Merge mixer_paths and mixer_gains into one file

--- device.mk
+++ device.mk
@@ -20,7 +20,6 @@ DEVICE_PATH := device/samsung/r8s
 
 # Audio
 PRODUCT_COPY_FILES += \
-    $(DEVICE_PATH)/configs/audio/mixer_gains.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_gains.xml \
     $(DEVICE_PATH)/configs/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml \
     $(DEVICE_PATH)/configs/audio/mixer_usb_default.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_usb_default.xml \
     $(DEVICE_PATH)/configs/audio/mixer_usb_gray.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_usb_gray.xml
